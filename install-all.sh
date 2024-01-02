3d41bc5878e9616fd6f25fa15d8eebfc74fbe01e
Dennis <nimbus272@gmail.com>
2024-01-02 01:36:04 -0500

    lol

--- install-all.sh
+++ install-all.sh
@@ -5,4 +5,4 @@ bash $HOME/post-install-scripts/zsh.sh
 #Now that we have zsh
 zsh $HOME/post-install-scripts/extra-packages.sh && \
 zsh $HOME/post-install-scripts/get-dotfiles.sh
-clear
+reset; exec sudo --login --user $USER
