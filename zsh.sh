9da9905ecaa61ac7832c6168fbcb41e57dda1c38
Dennis <nimbus272@gmail.com>
2024-01-02 02:24:01 -0500

    update zsh script

--- zsh.sh
+++ zsh.sh
@@ -9,4 +9,4 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 yay -S --noconfirm starship
 rm -rf ~/.zshrc
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/MichaelAquilina/zsh-you-should-use.git $ZSH_CUSTOM/plugins/you-should-use
+git clone https://github.com/MichaelAquilina/zsh-you-should-use.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/you-should-use
