6868e83ba5f4d026fd2163aa5230175e0704c0cc
nimbus272 <91817550+nimbus272@users.noreply.github.com>
2024-01-02 23:11:02 -0500

    Update yay.sh

--- yay.sh
+++ yay.sh
@@ -5,4 +5,5 @@ SCRIPT_USER=$1
 #install git and required base-devel tools (such as makepkg)
 pacman -S --noconfirm --needed base-devel git
 git clone https://aur.archlinux.org/yay.git /home/"$SCRIPT_USER"/yay
+chown -R bebbis:bebbis /home/"$SCRIPT_USER"/yay
 sudo -u "$SCRIPT_USER" bash -c "cd /home/$SCRIPT_USER/yay && makepkg -si --noconfirm"
