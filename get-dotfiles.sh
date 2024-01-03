81a6c3e1015c8ca22dddd30d9386c71b8e40c1b9
nimbus272 <91817550+nimbus272@users.noreply.github.com>
2024-01-02 23:48:04 -0500

    Update get-dotfiles.sh

--- get-dotfiles.sh
+++ get-dotfiles.sh
@@ -1,3 +1,5 @@
 SCRIPT_USER=$1
-git clone https://github.com/nimbus272/bebfiles.git /usr/local/stow/bebfiles
-stow -v -R -t /home/"$SCRIPT_USER" bebfiles
+git clone https://github.com/nimbus272/bebfiles.git /home/"$SCRIPT_USER"/bebfiles
+chown -R bebbis: /home/"$SCRIPT_USER"/bebfiles
+cd /home/"$SCRIPT_USER"/bebfiles
+stow -v -R -t /home/"$SCRIPT_USER" .
