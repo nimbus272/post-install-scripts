386d6dabe2c2b762dfa9c8214262b01b85c34b76
nimbus272 <91817550+nimbus272@users.noreply.github.com>
2024-01-02 23:58:50 -0500

    Update install-all.sh

--- install-all.sh
+++ install-all.sh
@@ -18,5 +18,4 @@ execute_script "https://raw.githubusercontent.com/nimbus272/post-install-scripts
 execute_script "https://raw.githubusercontent.com/nimbus272/post-install-scripts/main/extra-packages.sh"
 execute_script "https://raw.githubusercontent.com/nimbus272/post-install-scripts/main/get-dotfiles.sh"
 
-rm -rf /home/"$SCRIPT_USER"/.zshrc
 exec zsh
