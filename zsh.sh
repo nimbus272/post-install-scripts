863b5279bdb53aab88726b5e10f98a5a7a378381
nimbus272 <91817550+nimbus272@users.noreply.github.com>
2024-01-02 21:22:49 -0500

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -12,24 +12,27 @@ else
 fi
 
 #Install zsh and change default shell
-$PKG_MANAGER zsh
-chsh -s $(which zsh) $(whoami)
-
-# Install oh-my-zsh
-# No config should be necessary, we will clone dotfiles later
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc
-rm -rf /home/bebbis/.zshrc
-
-# Install starship
-if [ -f "/etc/arch-release" ]; then
-    yay -S --noconfirm starship
-elif [ -f "/etc/debian_version" ]; then
-    curl -fsSL https://starship.rs/install.sh | bash -s -- --yes
-elif [ -f "/etc/fedora-release" ]; then
-    $PKG_MANAGER starship
-else
-    echo "Unsupported distribution"
-    exit 1
-fi
-curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | bash
+sudo -u bebbis bash -c '\
+    "$PKG_MANAGER zsh"
+    chsh -s $(which zsh) $(whoami)
+    
+    # Install oh-my-zsh
+    # No config should be necessary, we will clone dotfiles later
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc
+    
+    # Install starship
+    if [ -f "/etc/arch-release" ]; then
+        yay -S --noconfirm starship
+    elif [ -f "/etc/debian_version" ]; then
+        curl -fsSL https://starship.rs/install.sh | bash -s -- --yes
+    elif [ -f "/etc/fedora-release" ]; then
+        $PKG_MANAGER starship
+    else
+        echo "Unsupported distribution"
+        exit 1
+    fi
+    rm -rf ~/.zshrc
+    #install zplug
+    curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | bash
+'
 
