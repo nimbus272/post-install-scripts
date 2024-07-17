56fe81313a841e28e466a8e520f018780f6217be
Dennis <nimbus272@gmail.com>
2024-07-16 22:17:19 -0400

    update extra package list

--- extra-packages.sh
+++ extra-packages.sh
@@ -1,20 +1,20 @@
 # Determine the package manager
 if [ -f "/etc/arch-release" ]; then
-    PKG_MANAGER="pacman -S --noconfirm"
-    PKG_QUERY="pacman -Qi"
+	PKG_MANAGER="pacman -S --noconfirm"
+	PKG_QUERY="pacman -Qi"
 elif [ -f "/etc/debian_version" ]; then
-    PKG_MANAGER="apt-get install -y"
-    PKG_QUERY="dpkg -s"
+	PKG_MANAGER="apt-get install -y"
+	PKG_QUERY="dpkg -s"
 elif [ -f "/etc/fedora-release" ]; then
-    PKG_MANAGER="dnf install -y"
-    PKG_QUERY="dnf list installed"
+	PKG_MANAGER="dnf install -y"
+	PKG_QUERY="dnf list installed"
 else
-    echo "Unsupported distribution"
-    exit 1
+	echo "Unsupported distribution"
+	exit 1
 fi
 
-for pkg in neofetch stow docker wget; do
-    if ! $PKG_QUERY "$pkg" &> /dev/null; then
-        $PKG_MANAGER "$pkg"
-    fi
+for pkg in fastfetch stow docker wget eza vivid; do
+	if ! $PKG_QUERY "$pkg" &>/dev/null; then
+		$PKG_MANAGER "$pkg"
+	fi
 done
